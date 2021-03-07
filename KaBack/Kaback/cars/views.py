f85e275ae93e19f35d6205bf9af8c21052129e74
Zigeel <170300001@ucaribe.edu.mx>
2021-03-07 13:02:35 -0500

    Creacion de rutas carsID y cars

--- KaBack/Kaback/cars/views.py
+++ KaBack/Kaback/cars/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render,HttpResponse
 def hola_mundo(request):
     return HttpResponse("HolaaaaaAAaaAAaaaAA")
 
+#si se seleciona ejemplo el carro 5 en la ruta, con el id debe demostrar el carro 5 
+def qryId(request,carId):
+    return HttpResponse("Id")
+
+def qryAll(request):
+    return HttpResponse("All cars, aqui se retorna los objetos de todos los carros")
 
 
