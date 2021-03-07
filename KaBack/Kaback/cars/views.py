5e19102aca9d884aa3a7c5db6ada385e69fde3c2
Zigeel <170300001@ucaribe.edu.mx>
2021-03-07 14:08:10 -0500

    ejemplo descripcion de objeto

--- KaBack/Kaback/cars/views.py
+++ KaBack/Kaback/cars/views.py
@@ -1,12 +1,14 @@
 
 from django.shortcuts import render,HttpResponse
+import json
+
 # Create your views here.
 def hola_mundo(request):
     return HttpResponse("HolaaaaaAAaaAAaaaAA")
 
 #si se seleciona ejemplo el carro 5 en la ruta, con el id debe demostrar el carro 5 
 def qryId(request,carId):
-    return HttpResponse("Id")
+    return HttpResponse(json.dumps({"id":carId, "nombre":"respuesta del carro"}), content_type="application/json")
 
 def qryAll(request):
     return HttpResponse("All cars, aqui se retorna los objetos de todos los carros")
